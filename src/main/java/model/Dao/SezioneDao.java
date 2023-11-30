f70990a00c844d8384a0b9ce933c225c90bca932
Andrea Caforio <andreacaforio74@gmail.com>
2023-11-30 16:59:49 +0100

    added dependency

--- src/main/java/model/Dao/SezioneDao.java
+++ src/main/java/model/Dao/SezioneDao.java
@@ -1,4 +1,5 @@
 package model.Dao;
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -9,141 +10,131 @@ import java.util.ArrayList;
 import java.util.List;
 
 //import com.mysql.cj.x.protobuf.MysqlxCrud.Order.Direction;
-//import com.mysql.cj.xdevapi.Statement;
 
+//import com.mysql.cj.xdevapi.Statement;
 
 import model.Bean.SezioneBean;
 import utils.DBConnection;
 
 public class SezioneDao {
 
+    public List<SezioneBean> findAll() throws ClassNotFoundException, SQLException {
 
-    public List<SezioneBean> findAll() throws ClassNotFoundException, SQLException{
+	List<SezioneBean> listaSez = new ArrayList<>();
 
-        List<SezioneBean> listaSez = new ArrayList<>();
+	DBConnection dBConn = new DBConnection();
+	Connection conn = dBConn.createConnection();
+	Statement stmt = conn.createStatement();
+	ResultSet rs = stmt.executeQuery("SELECT * FROM sezione");
 
-        DBConnection dBConn = new DBConnection();
-		Connection conn = dBConn.createConnection();
-		Statement stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT * FROM sezione");
+	while (rs.next()) {
 
-        while(rs.next()){
+	    SezioneBean sezioneBean = new SezioneBean();
 
-            SezioneBean sezioneBean = new SezioneBean();
-            
-            sezioneBean.setSezioneId(rs.getInt("sezione_id"));
-            sezioneBean.setTitoloSez(rs.getString("titolo_sez"));
-            sezioneBean.setDescrSez(rs.getString("descr_sez"));
-            sezioneBean.setDataUltAcc(rs.getDate("data_ult_acc"));
-            sezioneBean.setDataUltMod(rs.getDate("data_ult_mod"));
-            sezioneBean.setFlgDel(rs.getBoolean("flg_del"));
-        
-            listaSez.add(sezioneBean);
-            
-            
+	    sezioneBean.setSezioneId(rs.getInt("sezione_id"));
+	    sezioneBean.setTitoloSez(rs.getString("titolo_sez"));
+	    sezioneBean.setDescrSez(rs.getString("descr_sez"));
+	    sezioneBean.setDataUltAcc(rs.getDate("data_ult_acc"));
+	    sezioneBean.setDataUltMod(rs.getDate("data_ult_mod"));
+	    sezioneBean.setFlgDel(rs.getBoolean("flg_del"));
 
-        }
+	    listaSez.add(sezioneBean);
 
-        conn.close();
-        return listaSez;
+	}
 
+	conn.close();
+	return listaSez;
 
     }
 
-    public SezioneBean findById(int id) throws ClassNotFoundException, SQLException{
+    public SezioneBean findById(int id) throws ClassNotFoundException, SQLException {
 
-        SezioneBean sezioneBean = new SezioneBean();
-        DBConnection dBConn = new DBConnection();
-		Connection conn = dBConn.createConnection();
-		PreparedStatement ps = conn.prepareStatement("SELECT * FROM sezione WHERE sezione_id = ?");
-        ps.setFloat(1, id);
-        ResultSet rs = ps.executeQuery();
+	SezioneBean sezioneBean = new SezioneBean();
+	DBConnection dBConn = new DBConnection();
+	Connection conn = dBConn.createConnection();
+	PreparedStatement ps = conn.prepareStatement("SELECT * FROM sezione WHERE sezione_id = ?");
+	ps.setFloat(1, id);
+	ResultSet rs = ps.executeQuery();
 
-        while (rs.next()){
+	while (rs.next()) {
 
-            sezioneBean.setSezioneId(id);
-            sezioneBean.setTitoloSez(rs.getString("titolo_sezione"));
-            sezioneBean.setDescrSez(rs.getString("descr_sez"));
-            sezioneBean.setDataUltAcc(rs.getDate("data_ult_acc"));
-            sezioneBean.setDataUltMod(rs.getDate("data_ult_mod"));
-            sezioneBean.setFlgDel(rs.getBoolean("flg_del"));
-        
-        }
+	    sezioneBean.setSezioneId(id);
+	    sezioneBean.setTitoloSez(rs.getString("titolo_sezione"));
+	    sezioneBean.setDescrSez(rs.getString("descr_sez"));
+	    sezioneBean.setDataUltAcc(rs.getDate("data_ult_acc"));
+	    sezioneBean.setDataUltMod(rs.getDate("data_ult_mod"));
+	    sezioneBean.setFlgDel(rs.getBoolean("flg_del"));
 
-        conn.close();
-        return sezioneBean;
+	}
+
+	conn.close();
+	return sezioneBean;
     }
 
+    public int create(int sezioneId, String titoloSez, String descrSez, Date dataUltAcc, Date dataUltMod,
+	    Boolean flgDel) throws ClassNotFoundException, SQLException {
+	// TEST
+	Connection conn = DBConnection.createConnection();
+	PreparedStatement ps = conn.prepareStatement(
+		"INSERT INTO sezione (sezione_id, titolo_sez, descr_sez, data_ult_acc, data_ult_mod, flg_del) VALUES (?, ?, ?, ?, ?, ?)");
 
-    public int create(int sezioneId, String titoloSez, String descrSez, Date dataUltAcc, Date dataUltMod, Boolean flgDel) throws ClassNotFoundException, SQLException {
-        //TEST
-		Connection conn = DBConnection.createConnection();
-		PreparedStatement ps = conn.prepareStatement("INSERT INTO sezione (sezione_id, titolo_sez, descr_sez, data_ult_acc, data_ult_mod, flg_del) VALUES (?, ?, ?, ?, ?, ?)");
+	ps.setInt(1, sezioneId);
+	ps.setString(2, titoloSez);
+	ps.setString(3, descrSez);
+	ps.setDate(4, dataUltAcc);
+	ps.setDate(5, dataUltMod);
+	ps.setBoolean(6, flgDel);
 
-		ps.setInt(1, sezioneId);
-		ps.setString(2, titoloSez);
-        ps.setString(3, descrSez);
-		ps.setDate(4, dataUltAcc);
-		ps.setDate(5, dataUltMod);
-		ps.setBoolean(6, flgDel);
-		
+	int execute_success = ps.executeUpdate();
+	conn.close();
 
-		int execute_success = ps.executeUpdate();
-		conn.close();
+	return execute_success;
+    }
 
-		return execute_success;
-	}
+    public int update(int sezioneId, String titoloSez, String descrSez, Date dataUltAcc, Date dataUltMod,
+	    Boolean flgDel) throws ClassNotFoundException, SQLException {
 
+	Connection conn = DBConnection.createConnection();
+	PreparedStatement ps = conn.prepareStatement(
+		"UPDATE sezione SET sezione_id = ?, titolo_sez = ?, descr_sez = ?, data_ult_acc = ?, data_ult_mod = ?, flg_del = ?  WHERE sezione_id = ?");
 
+	ps.setInt(1, sezioneId);
+	ps.setString(2, titoloSez);
+	ps.setString(3, descrSez);
+	ps.setDate(4, dataUltAcc);
+	ps.setDate(5, dataUltMod);
+	ps.setBoolean(6, flgDel);
 
-	public int update(int sezioneId, String titoloSez, String descrSez, Date dataUltAcc, Date dataUltMod, Boolean flgDel) throws ClassNotFoundException, SQLException {
+	int execute_success = ps.executeUpdate();
+	conn.close();
 
-		Connection conn = DBConnection.createConnection();
-		PreparedStatement ps = conn.prepareStatement(
-				"UPDATE sezione SET sezione_id = ?, titolo_sez = ?, descr_sez = ?, data_ult_acc = ?, data_ult_mod = ?, flg_del = ?  WHERE sezione_id = ?");
+	return execute_success;
+    }
 
-		ps.setInt(1, sezioneId);
-		ps.setString(2, titoloSez);
-		ps.setString(3, descrSez);
-		ps.setDate(4, dataUltAcc);
-        ps.setDate(5, dataUltMod);
-		ps.setBoolean(6, flgDel);
-	
-		int execute_success = ps.executeUpdate();
-		conn.close();
+    // logic delete
+    public int logicDeleteBook(int sezioneId) throws ClassNotFoundException, SQLException {
 
-		return execute_success;
-	}
+	Connection conn = DBConnection.createConnection();
+	PreparedStatement ps = conn.prepareStatement("UPDATE sezione SET flg_Del = true WHERE sezione_id = ?");
+	ps.setInt(1, sezioneId);
 
-	// logic delete
-	public int logicDeleteBook(int sezioneId) throws ClassNotFoundException, SQLException {
+	int execute_success = ps.executeUpdate();
+	conn.close();
 
-		Connection conn = DBConnection.createConnection();
-		PreparedStatement ps = conn.prepareStatement("UPDATE sezione SET flg_Del = true WHERE sezione_id = ?");
-		ps.setInt(1, sezioneId);
+	return execute_success;
+    }
 
-		int execute_success = ps.executeUpdate();
-		conn.close();
+    // physical delete
+    public int deleteBook(int sezioneId) throws ClassNotFoundException, SQLException {
 
-		return execute_success;
-	}
+	Connection conn = DBConnection.createConnection();
+	PreparedStatement ps = conn.prepareStatement("DELETE FROM sezione WHERE sezione_id = ?");
+	ps.setInt(1, sezioneId);
 
-	// physical delete
-	public int deleteBook(int sezioneId) throws ClassNotFoundException, SQLException {
+	int execute_success = ps.executeUpdate();
+	conn.close();
 
-		Connection conn = DBConnection.createConnection();
-		PreparedStatement ps = conn.prepareStatement("DELETE FROM sezione WHERE sezione_id = ?");
-		ps.setInt(1, sezioneId);
-
-		int execute_success = ps.executeUpdate();
-		conn.close();
-
-		return execute_success;
-	}
+	return execute_success;
+    }
 
 }
-
-
-
-
-
