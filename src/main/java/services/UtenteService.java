027693e9e07ad37b55ae3ba9345111b6c5eceac6
albertpaner <albertpanerai@gmail.com>
2023-12-05 09:34:58 +0100

    utenteService hotfix

--- src/main/java/services/UtenteService.java
+++ src/main/java/services/UtenteService.java
@@ -2,6 +2,8 @@ package services;
 
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -59,5 +61,9 @@ public class UtenteService {
 	}
 
     }
+
+
+
+
 
 }
