4d112f6c37916293cea825f46eef3d24a1d1182f
NBK435LEH3QQ72B\User <dinucci@live.it>
2023-12-06 11:08:40 +0100

    CountDto

---
+++ src/main/java/model/Dto/CountDto.java
@@ -0,0 +1,46 @@
+package model.Dto;
+
+
+public class CountDto {
+
+    private int ValutatoreId;
+    private String nome;
+    private String cognome;
+    private String email;
+    private int count;
+    
+    
+	public int getValutatoreId() {
+		return ValutatoreId;
+	}
+	public void setValutatoreId(int valutatoreId) {
+		ValutatoreId = valutatoreId;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+    
+    
+    
+}
