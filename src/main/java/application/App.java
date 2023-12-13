3899f184b5e3c7edf4486139a49ef3dc9e46a221
albertpaner <albertpanerai@gmail.com>
2023-12-13 11:20:13 +0100

    testing BE

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -15,17 +15,30 @@ public class App {
 
         UtenteDao utenteDao = new UtenteDao();
         UtenteEvaluationService utenteEvaluationService = new UtenteEvaluationService(utenteDao);
-        HashMap<String, List<EvalCountDto>> evaluators = utenteEvaluationService.getEvaluatorsOccupiedFree(3);
+
+        int soglia = 3;
+        HashMap<String, List<EvalCountDto>> evaluators = utenteEvaluationService.getEvaluatorsOccupiedFree(soglia);
         List<EvalCountDto> freeEvaluators = evaluators.get("disponibili");
         List<EvalCountDto> occupiedEvaluators = evaluators.get("occupati");
 
+        System.out.println("before equilibrateValutatori");
         System.out.println("occupiedEvaluators: " + occupiedEvaluators);
         System.out.println("--------------------------------------------");
         System.out.println("freeEvaluators: " + freeEvaluators);
         System.out.println("--------------------------------------------");
 
-        int res = utenteEvaluationService.equilibrateValutatori(evaluators, 3);
+        int res = utenteEvaluationService.equilibrateValutatori(evaluators, soglia);
         System.out.println("res: " + res);
+
+        evaluators = utenteEvaluationService.getEvaluatorsOccupiedFree(soglia);
+        freeEvaluators = evaluators.get("disponibili");
+        occupiedEvaluators = evaluators.get("occupati");
+
+        System.out.println("after equilibrateValutatori");
+        System.out.println("occupiedEvaluators: " + occupiedEvaluators);
+        System.out.println("--------------------------------------------");
+        System.out.println("freeEvaluators: " + freeEvaluators);
+
     }
 
 }
